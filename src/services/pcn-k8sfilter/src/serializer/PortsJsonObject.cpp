4508ebb95b51fe88f60ca6c67fa28fd422955bd0
Fylax <nico.caprioli@gmail.com>
2019-01-29 20:56:42 +0100

    Ported k8sfilter
    Signed-off-by: Nico Caprioli <nico.caprioli@gmail.com>

--- src/services/pcn-k8sfilter/src/serializer/PortsJsonObject.cpp
+++ src/services/pcn-k8sfilter/src/serializer/PortsJsonObject.cpp
@@ -22,52 +22,40 @@ namespace swagger {
 namespace server {
 namespace model {
 
-PortsJsonObject::PortsJsonObject() {
+PortsJsonObject::PortsJsonObject() :
+  m_nameIsSet (false),
+  m_uuidIsSet (false),
+  m_statusIsSet (false),
+  m_peerIsSet (false),
+  m_typeIsSet (false) { }
 
-  m_nameIsSet = false;
+PortsJsonObject::PortsJsonObject(nlohmann::json& val) :
+  m_nameIsSet (false),
+  m_uuidIsSet (false),
+  m_statusIsSet (false),
+  m_peerIsSet (false),
+  // Mandatory item
+  m_type (string_to_PortsTypeEnum(val.at("type").get<std::string>())),
+  m_typeIsSet (true) {
 
-  m_uuidIsSet = false;
-
-  m_statusIsSet = false;
-
-  m_peerIsSet = false;
-
-  m_typeIsSet = false;
-}
-
-PortsJsonObject::~PortsJsonObject() {}
-
-void PortsJsonObject::validateKeys() {
-
-  if (!m_nameIsSet) {
-    throw std::runtime_error("Variable name is required");
+  if (val.count("uuid") != 0) {
+    setUuid(val.at("uuid").get<std::string>());
   }
-}
 
-void PortsJsonObject::validateMandatoryFields() {
-
-  if (!m_typeIsSet) {
-    throw std::runtime_error("Variable type is required");
+  if (val.count("status") != 0) {
+    setStatus(string_to_PortsStatusEnum(val.at("status").get<std::string>()));
   }
-}
 
-void PortsJsonObject::validateParams() {
-
-  if (m_uuidIsSet) {
-    std::string patter_value = R"PATTERN([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})PATTERN";
-    std::regex e (patter_value);
-    if (!std::regex_match(m_uuid, e))
-      throw std::runtime_error("Variable uuid has not a valid format");
+  if (val.count("peer") != 0) {
+    setPeer(val.at("peer").get<std::string>());
   }
+
 }
 
 nlohmann::json PortsJsonObject::toJson() const {
   nlohmann::json val = nlohmann::json::object();
 
-  if (m_nameIsSet) {
-    val["name"] = m_name;
-  }
-
+  val["name"] = m_name;
   if (m_uuidIsSet) {
     val["uuid"] = m_uuid;
   }
@@ -83,37 +71,6 @@ nlohmann::json PortsJsonObject::toJson() const {
   val["type"] = PortsTypeEnum_to_string(m_type);
 
   return val;
-}
-
-void PortsJsonObject::fromJson(nlohmann::json& val) {
-  for(nlohmann::json::iterator it = val.begin(); it != val.end(); ++it) {
-    std::string key = it.key();
-    bool found = (std::find(allowedParameters_.begin(), allowedParameters_.end(), key) != allowedParameters_.end());
-    if (!found) {
-      throw std::runtime_error(key + " is not a valid parameter");
-      return;
-    }
-  }
-
-  if (val.find("name") != val.end()) {
-    setName(val.at("name"));
-  }
-
-  if (val.find("uuid") != val.end()) {
-    setUuid(val.at("uuid"));
-  }
-
-  if (val.find("status") != val.end()) {
-    setStatus(string_to_PortsStatusEnum(val.at("status")));
-  }
-
-  if (val.find("peer") != val.end()) {
-    setPeer(val.at("peer"));
-  }
-
-  if (val.find("type") != val.end()) {
-    setType(string_to_PortsTypeEnum(val.at("type")));
-  }
 }
 
 nlohmann::json PortsJsonObject::helpKeys() {
@@ -191,9 +148,7 @@ bool PortsJsonObject::nameIsSet() const {
   return m_nameIsSet;
 }
 
-void PortsJsonObject::unsetName() {
-  m_nameIsSet = false;
-}
+
 
 
 
@@ -285,9 +240,7 @@ bool PortsJsonObject::typeIsSet() const {
   return m_typeIsSet;
 }
 
-void PortsJsonObject::unsetType() {
-  m_typeIsSet = false;
-}
+
 
 std::string PortsJsonObject::PortsTypeEnum_to_string(const PortsTypeEnum &value){
   switch(value){
@@ -314,4 +267,5 @@ PortsTypeEnum PortsJsonObject::string_to_PortsTypeEnum(const std::string &str){
 }
 }
 }
+
 
